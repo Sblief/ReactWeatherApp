94a7471e594317dc1ab51a2e5f6bbee1e5bbc733
shibli16 <emon744254>
2017-07-03 02:07:54 +0600

    Heroku deploy

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ var express = require("express");
 //create our app 
 var app = express();
 
+const PORT = process.env.PORT || 300;
+
+app.use(function (req, res, next) {			
+	if (req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect('http://'+req.hostname+ req.url);
+	}
+	
+});
+
 app.use(express.static('public'));
 
-app.listen(300, function() {
-	console.log('Express server is running on port 300');
+app.listen(PORT, function() {
+	console.log('Express server is running on port' + PORT);
 });
