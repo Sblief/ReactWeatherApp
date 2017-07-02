dfbfd8bf195080be43fb86d66151c05db4e10bee
shibli16 <emon744254>
2017-07-02 12:21:25 +0600

    Refractor

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,12 +1,11 @@
 var React = require('react');
 
 
-var WeatherMessage = React.createClass({
-	render: function () {
+var WeatherMessage = ({temp, location}) => {
 		return (
-			<h3>It's 36 in Dhaka</h3>
+			<h3>It's {temp} in {location}</h3>
 			);
-	}
-});
+	};
+
 
 module.exports  = WeatherMessage;
---
+++ arrow.js
@@ -0,0 +1,36 @@
+var names = ['Rahim', 'Karim', 'Abul'];
+
+// names.forEach(function (name) {
+// 	console.log('forEach', name);
+// });
+
+// names.forEach((name)  =>  {
+// 	console.log('Arrow', name);
+// });
+
+
+// names.forEach((name)  => console.log(name));
+
+
+// var ReturnValue = (name) => name+'!';
+
+// console.log(ReturnValue('Shibli'));
+// 
+
+// var user = {
+// 	name: 'Shibli',
+// 	hello: function () {
+// 		names.forEach( (name) => {
+// 			console.log(this.name+' says hello to '+name)
+// 		});
+// 	}
+// };
+
+
+// user.hello();
+// 
+// 
+
+// var Add = (a,b) => a+b;
+
+// console.log(Add(56,4));
